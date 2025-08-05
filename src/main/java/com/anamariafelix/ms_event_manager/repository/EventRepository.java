3b012b8d6a574a0409f91bb5692b9f09525d5c5d
user.name <anafelix0909@gmail.com>
2025-08-05 12:14:07 -0300

    feat: creation of the EventRepository  interface

---
+++ src/main/java/com/anamariafelix/ms_event_manager/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package com.anamariafelix.ms_event_manager.repository;
+
+import com.anamariafelix.ms_event_manager.model.Event;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends MongoRepository<Event, String> {
+}
