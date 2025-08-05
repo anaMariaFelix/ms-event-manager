98d4b3f8ee7f272bf2f0b3c6a0c2b7b75f003c07
user.name <anafelix0909@gmail.com>
2025-08-05 12:30:45 -0300

    feat: Creating the Client for ViaCEP

---
+++ src/main/java/com/anamariafelix/ms_event_manager/infra/ViaCepClientOpenFeign.java
@@ -0,0 +1,13 @@
+package com.anamariafelix.ms_event_manager.infra;
+
+import com.anamariafelix.ms_event_manager.dto.ViaCepResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepClientOpenFeign {
+
+    @GetMapping("/{cep}/json/")
+    ViaCepResponseDTO findByAddress(@PathVariable("cep") String cep);
+}
