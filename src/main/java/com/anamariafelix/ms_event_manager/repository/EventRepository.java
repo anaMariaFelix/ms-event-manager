4e688fec440b371507d5672dfc047659858e1403
user.name <anafelix0909@gmail.com>
2025-08-05 16:57:53 -0300

    feat: add findAllByOrderByEventNameAsc query method in eventRepository

--- src/main/java/com/anamariafelix/ms_event_manager/repository/EventRepository.java
+++ src/main/java/com/anamariafelix/ms_event_manager/repository/EventRepository.java
@@ -4,6 +4,10 @@ import com.anamariafelix.ms_event_manager.model.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventRepository extends MongoRepository<Event, String> {
+
+    List<Event> findAllByOrderByEventNameAsc();
 }
