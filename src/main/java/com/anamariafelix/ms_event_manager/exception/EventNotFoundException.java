2f1f1bdec3ceaeb63ce4381319f602af198d44e5
user.name <anafelix0909@gmail.com>
2025-08-05 15:45:59 -0300

    feat: custom exception implementation

---
+++ src/main/java/com/anamariafelix/ms_event_manager/exception/EventNotFoundException.java
@@ -0,0 +1,8 @@
+package com.anamariafelix.ms_event_manager.exception;
+
+public class EventNotFoundException extends RuntimeException {
+
+    public EventNotFoundException(String msg) {
+        super(msg);
+    }
+}
