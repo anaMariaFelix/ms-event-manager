5475bc9f49d2c361bca4e5bf98bb37d01294c05e
user.name <anafelix0909@gmail.com>
2025-08-05 15:44:37 -0300

    feat: implementation of the findById method

--- src/main/java/com/anamariafelix/ms_event_manager/controller/EventController.java
+++ src/main/java/com/anamariafelix/ms_event_manager/controller/EventController.java
@@ -6,11 +6,10 @@ import com.anamariafelix.ms_event_manager.model.Event;
 import com.anamariafelix.ms_event_manager.service.EventService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import static com.anamariafelix.ms_event_manager.mapper.EventMapper.toEvent;
 import static com.anamariafelix.ms_event_manager.mapper.EventMapper.toEventDTO;
@@ -27,6 +26,12 @@ public class EventController {
         Event event = eventService.create(toEvent(eventCreateDTO));
 
         return ResponseEntity.status(201).body(toEventDTO(event));
+    }
+
+    @GetMapping("/get-event/{id}")
+    public ResponseEntity<EventResponseDTO> findById(@PathVariable String id) {
+        Event event = eventService.fidById(id);
+        return ResponseEntity.ok().body(toEventDTO(event));
     }
 
 }
