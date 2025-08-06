4dc2bb236cb75b17b6a00ebaf0779248c7f95779
user.name <anafelix0909@gmail.com>
2025-08-06 14:54:11 -0300

    feat: adding permissions with @PreAuthorize in the EventController class

--- src/main/java/com/anamariafelix/ms_event_manager/controller/EventController.java
+++ src/main/java/com/anamariafelix/ms_event_manager/controller/EventController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +18,12 @@ import static com.anamariafelix.ms_event_manager.mapper.EventMapper.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/events")
+@RequestMapping("/api/v1/events")
 public class EventController {
 
     private final EventService eventService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create-event")
     public ResponseEntity<EventResponseDTO> create(@RequestBody @Valid EventCreateDTO eventCreateDTO) {
         Event event = eventService.create(toEvent(eventCreateDTO));
@@ -29,28 +31,31 @@ public class EventController {
         return ResponseEntity.status(201).body(toEventDTO(event));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/get-event/{id}")
     public ResponseEntity<EventResponseDTO> findById(@PathVariable String id) {
         Event event = eventService.fidById(id);
         return ResponseEntity.ok().body(toEventDTO(event));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/get-all-events")
     public ResponseEntity<List<EventResponseDTO>> findAll() {
         List<Event> events = eventService.fidAll();
         return ResponseEntity.ok().body(toListEventDTO(events));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/get-all-events/sorted")
     public ResponseEntity<List<EventResponseDTO>> findAllSorted() {
         List<Event> events = eventService.fidAllSorted();
         return ResponseEntity.ok().body(toListEventDTO(events));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/update-event/{id}")
     public ResponseEntity<EventResponseDTO> update(@PathVariable String id, @RequestBody @Valid EventCreateDTO eventCreateDTO) {
         Event event = eventService.update(id, toEvent(eventCreateDTO));
         return ResponseEntity.ok().body(toEventDTO(event));
     }
-
 }
