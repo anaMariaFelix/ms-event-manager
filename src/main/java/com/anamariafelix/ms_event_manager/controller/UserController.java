c2b48c65189d017e09d1b5f3d418a8ab5cc6fbdc
user.name <anafelix0909@gmail.com>
2025-08-06 00:44:19 -0300

    feat: creating the UserController class with the create endpoint

---
+++ src/main/java/com/anamariafelix/ms_event_manager/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.anamariafelix.ms_event_manager.controller;
+
+import com.anamariafelix.ms_event_manager.dto.UserCreateDTO;
+import com.anamariafelix.ms_event_manager.dto.UserResponseDTO;
+import com.anamariafelix.ms_event_manager.model.User;
+import com.anamariafelix.ms_event_manager.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.anamariafelix.ms_event_manager.mapper.UserMapper.toUser;
+import static com.anamariafelix.ms_event_manager.mapper.UserMapper.toUserDTO;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/create-user")
+    public ResponseEntity<UserResponseDTO> create(@RequestBody @Valid UserCreateDTO userCreateDTO) {
+        User user = userService.create(toUser(userCreateDTO));
+
+        return ResponseEntity.status(201).body(toUserDTO(user));
+    }
+}
