c3a2c954afd1300cd4ca845c36e0319bce9b227b
user.name <anafelix0909@gmail.com>
2025-08-05 21:45:54 -0300

    feat: implementation of the findByEventNameAndDateTime method in EventRepository

--- src/main/java/com/anamariafelix/ms_event_manager/repository/EventRepository.java
+++ src/main/java/com/anamariafelix/ms_event_manager/repository/EventRepository.java
@@ -4,10 +4,14 @@ import com.anamariafelix.ms_event_manager.model.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EventRepository extends MongoRepository<Event, String> {
 
     List<Event> findAllByOrderByEventNameAsc();
+
+    Event findByEventNameAndDateTime(String eventName, LocalDateTime dateTime);
 }
