89ffcfbbad77bf5259bbb705eb512efd7010d913
user.name <anafelix0909@gmail.com>
2025-08-09 20:18:07 -0300

    feat: Documentation implementation of the create endpoint of the UserController class

---
+++ src/main/java/com/anamariafelix/ms_event_manager/controller/docs/UserControllerDocs.java
@@ -0,0 +1,27 @@
+package com.anamariafelix.ms_event_manager.controller.docs;
+
+import com.anamariafelix.ms_event_manager.controller.exception.ErrorMessage;
+import com.anamariafelix.ms_event_manager.dto.UserCreateDTO;
+import com.anamariafelix.ms_event_manager.dto.UserResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface UserControllerDocs {
+
+    @Operation(summary = "Create a new user", description = "Resources for creating a new user",
+            tags = {"User"},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Resource created successfully",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDTO.class))),
+                    @ApiResponse(responseCode = "409", description = "Email user already registered in the system",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Resource not processed due to invalid input data",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
+    ResponseEntity<UserResponseDTO> create(@RequestBody @Valid UserCreateDTO userCreateDTO);
+}
